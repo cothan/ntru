2c96b7b6fddf596857039237fdc55060f23fec3f
cothan <cothannguyen@gmail.com>
2020-02-28 01:46:04 -0500

    TODO: write main function to a function name

--- NEON/asimd-hps4096821/asmgen/asimd_poly_r2_mul.py
+++ NEON/asimd-hps4096821/asmgen/asimd_poly_r2_mul.py
@@ -1,19 +1,20 @@
 p = print
 
+
 def mult_128x128(out, x, y, t1, t2, t3):
     # Guarantee not modify x, y registers
     xxyy, xy = out
-    
-    p("vmull_p64 (y{}, y{}) = y{}".format(x, y, xy)) # x0, y0 -> t0
 
-    p("vmull_high_p64 (y{}, y{}) = y{}".format(x, y, xxyy)) # x1 * y1 -> t2 
+    p("vmull_p64 (y{}, y{}) = y{}".format(x, y, xy))  # x0, y0 -> t0
+
+    p("vmull_high_p64 (y{}, y{}) = y{}".format(x, y, xxyy))  # x1 * y1 -> t2
     #####################
-    # y1| y0 -> y0| y1 
+    # y1| y0 -> y0| y1
     p("vextq_p64(y{}, y{}, 1) = y{}".format(y, y, t3))
-    # t3 = y0|y1 
-    p("vmull_p64 ( y{}, y{} ) = y{}".format(x, t3, t1)) # x0 *  y1 -> t1
+    # t3 = y0|y1
+    p("vmull_p64 ( y{}, y{} ) = y{}".format(x, t3, t1))  # x0 *  y1 -> t1
 
-    p("vmull_high_p64 (y{}, y{}) = y{}".format(x, t3, t2)) # x1 * y0 -> t2
+    p("vmull_high_p64 (y{}, y{}) = y{}".format(x, t3, t2))  # x1 * y0 -> t2
 
     # t1 = t1 + t2
     p("vaddq_p64(y{}, y{}) = y{}".format(t1, t2, t1))
@@ -26,13 +27,13 @@ def mult_128x128(out, x, y, t1, t2, t3):
     p("vextq_p64(y{}, y{}, 1) = y{}".format(t1, t2, t3))
 
     #  xy = t2(16) + xy
-    p("vaddq_p64(y{}, y{}) = y{}".format(t3, xy, xy)) # out_low
+    p("vaddq_p64(y{}, y{}) = y{}".format(t3, xy, xy))  # out_low
 
     # Aligned t2(16) = (t1, t2)
     p("vextq_p64(y{}, y{}, 1) = y{}".format(t2, t1, t3))
 
     # xxyy = t2(16) + xxyy
-    p("vaddq_p64(y{}, y{}) = y{}".format(t3, xxyy, xxyy)) # out_high
+    p("vaddq_p64(y{}, y{}) = y{}".format(t3, xxyy, xxyy))  # out_high
 
     out = (xxyy, xy)
 
@@ -41,32 +42,32 @@ def karatsuba_256x256(ab, a_in, b_in, t0, t1, t2, t3, t4, t5, t6):
     """output: assumes a and b are two xmm low registers"""
     """output: assumes aa and bb are two xmm high registers"""
 
-    # z00, z0 = a 
-    # z22, z2 = b 
+    # z00, z0 = a
+    # z22, z2 = b
     z00, z0, z22, z2 = ab
-    
+
     aa, a = a_in
     bb, b = b_in
 
-    t22 = t5 
-    t33 = t6 
-    
+    t22 = t5
+    t33 = t6
+
     # aa, bb = high(a), high(b)
     # b = low(out); t22 = high(out)
     mult_128x128((z22, z2), aa, bb, t2, t3, t4)
 
     p("vaddq_p128 (y{}, y{}) = y{}".format(aa, a, t0))
     p("vaddq_p128 (y{}, y{}) = y{}".format(bb, b, t1))
-    
-    mult_128x128( (t22, t2), t0, t1, t3, t33, t4)
-    mult_128x128( (z00, z0), a, b, t3, t33, t4)
+
+    mult_128x128((t22, t2), t0, t1, t3, t33, t4)
+    mult_128x128((z00, z0), a, b, t3, t33, t4)
 
     p("vaddq_p128  (y{}, y{}) = y{}".format(t22, z22, t22))
     p("vaddq_p128  (y{}, y{}) = y{}".format(t2, z2, t2))
 
     p("vaddq_p128  (y{}, y{}) = y{}".format(t22, z00, t22))
     p("vaddq_p128  (y{}, y{}) = y{}".format(t2, z0, t2))
-    
+
     # p("vpxor y{}, y{}, y{}".format(z1, a, z1))
     p("vaddq_p128  (y{}, y{}) = y{}".format(z22, t22, z22))
     p("vaddq_p128  (y{}, y{}) = y{}".format(z00, t2, z00))
@@ -77,7 +78,7 @@ def karatsuba_256x256(ab, a_in, b_in, t0, t1, t2, t3, t4, t5, t6):
 def karatsuba_512x512(w, ab_in, xy_in, t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10):
     """ w: 4 ymm reg. ab: 2 ymm reg. xy: 2 ymm reg. t*: 1 ymm reg """
     """ w: 8 xmm reg. ab: 4 xmm reg. xy: 4 xmm reg. t*: 1 ymm reg """
-    w00, w0, w11, w1, w22, w2, w33, w3 = w 
+    w00, w0, w11, w1, w22, w2, w33, w3 = w
 
     (aa, a, bb, b) = ab_in
     (xx, x, yy, y) = xy_in
@@ -91,9 +92,12 @@ def karatsuba_512x512(w, ab_in, xy_in, t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t
     p("vaddq_p128 (y{}, y{}) = y{}".format(x, y, t6))
     p("vaddq_p128 (y{}, y{}) = y{}".format(xx, yy, t66))
 
-    karatsuba_256x256( (w00, w0, w11, w1), (aa, a), (xx, x), t0, t1, t2, t3, t4, t7, t8)
-    karatsuba_256x256( (w22, w2, w33, w3), (bb, b), (yy, y), t0, t1, t2, t3, t4, t7, t8)
-    karatsuba_256x256( (aa, a, bb, b), (t55, t5), (t66, t6), t0, t1, t2, t3, t4, t7, t8)
+    karatsuba_256x256((w00, w0, w11, w1), (aa, a),
+                      (xx, x), t0, t1, t2, t3, t4, t7, t8)
+    karatsuba_256x256((w22, w2, w33, w3), (bb, b),
+                      (yy, y), t0, t1, t2, t3, t4, t7, t8)
+    karatsuba_256x256((aa, a, bb, b), (t55, t5), (t66, t6),
+                      t0, t1, t2, t3, t4, t7, t8)
 
     p("vaddq_p128 (y{}, y{}) = y{}".format(w00, aa, aa))
     p("vaddq_p128 (y{}, y{}) = y{}".format(w0, a, a))
@@ -113,8 +117,9 @@ def karatsuba_512x512(w, ab_in, xy_in, t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t
 
     w = (w00, w0, w11, w1, w22, w2, w33, w3)
 
+
 def store_1024(w, ptr="%rdi"):
-    w00, w0, w11, w1, w22, w2, w33, w3 = w 
+    w00, w0, w11, w1, w22, w2, w33, w3 = w
     p("vst1q_p16 ({} + {}, y{});".format(16*0, ptr, w00))
     p("vst1q_p16 ({} + {}, y{});".format(16*1, ptr, w0))
     p("vst1q_p16 ({} + {}, y{});".format(16*2, ptr, w11))
@@ -127,18 +132,20 @@ def store_1024(w, ptr="%rdi"):
 
     w = (w00, w0, w11, w1, w22, w2, w33, w3)
 
+
 def load_1024(w, ptr="%rdi"):
     w00, w0, w11, w1, w22, w2, w33, w3 = w
-    p("vld1q_p16 ({} + {}) = y{}".format(16*0, ptr, w00) ) 
-    p("vld1q_p16 ({} + {}) = y{}".format(16*1, ptr, w0) ) 
-    p("vld1q_p16 ({} + {}) = y{}".format(16*2, ptr, w11) )
-    p("vld1q_p16 ({} + {}) = y{}".format(16*3, ptr, w1) )  
-    p("vld1q_p16 ({} + {}) = y{}".format(16*4, ptr, w22) ) 
-    p("vld1q_p16 ({} + {}) = y{}".format(16*5, ptr, w2) ) 
-    p("vld1q_p16 ({} + {}) = y{}".format(16*6, ptr, w33) ) 
-    p("vld1q_p16 ({} + {}) = y{}".format(16*7, ptr, w3) ) 
+    p("vld1q_p16 ({} + {}) = y{}".format(16*0, ptr, w00))
+    p("vld1q_p16 ({} + {}) = y{}".format(16*1, ptr, w0))
+    p("vld1q_p16 ({} + {}) = y{}".format(16*2, ptr, w11))
+    p("vld1q_p16 ({} + {}) = y{}".format(16*3, ptr, w1))
+    p("vld1q_p16 ({} + {}) = y{}".format(16*4, ptr, w22))
+    p("vld1q_p16 ({} + {}) = y{}".format(16*5, ptr, w2))
+    p("vld1q_p16 ({} + {}) = y{}".format(16*6, ptr, w33))
+    p("vld1q_p16 ({} + {}) = y{}".format(16*7, ptr, w3))
 
     w = (w00, w0, w11, w1, w22, w2, w33, w3)
+
 
 def vec256_sr53(r_out, a_in, t0, t1):
     rr, r = r_out
@@ -153,16 +160,17 @@ def vec256_sr53(r_out, a_in, t0, t1):
     p("y{} << 11 = y{}".format(r, r))
 
     # t0 = a3|a2 , t1 = a1|a4
-    p("vextq_p64 (y{}, y{}, 1) = y{}".format(r, rr, t0) )
-    p("vextq_p64 (y{}, y{}, 1) = y{}".format(rr, r, t1) )
+    p("vextq_p64 (y{}, y{}, 1) = y{}".format(r, rr, t0))
+    p("vextq_p64 (y{}, y{}, 1) = y{}".format(rr, r, t1))
 
     p("y{} >> 53 = y{}".format(a, r))
     p("y{} >> 53 = y{}".format(aa, rr))
 
-    p("vaddq_p128  (y{}, y{}) = {}".format(r, t0, r) )
-    p("vaddq_p128  (y{}, y{}) = {}".format(rr, t1, rr ))
+    p("vaddq_p128  (y{}, y{}) = {}".format(r, t0, r))
+    p("vaddq_p128  (y{}, y{}) = {}".format(rr, t1, rr))
 
     r_out = (rr, r)
+
 
 def vec256_sl203(r_out, a_in, t0, t1):
     _, a = a_in
@@ -174,8 +182,8 @@ def vec256_sl203(r_out, a_in, t0, t1):
     p("y{} & 0xffffffffffffffff = y{}".format(a, r))
 
     # p("vpermq ${}, y{}, y{}".format(int('00''11''10''01', 2), r, r))
-    p("vextq_p64 (y{}, y{}, 1) = y{}".format(r, rr, t0) )
-    p("vextq_p64 (y{}, y{}, 1) = y{}".format(rr, r, t1) )
+    p("vextq_p64 (y{}, y{}, 1) = y{}".format(r, rr, t0))
+    p("vextq_p64 (y{}, y{}, 1) = y{}".format(rr, r, t1))
 
     # p("vpsllq ${}, y{}, y{}".format(11, r, r))
     p("y{} << 11 = y{}".format(t0, r))
@@ -183,45 +191,116 @@ def vec256_sl203(r_out, a_in, t0, t1):
 
     r_out = (rr, r)
 
+
 def mul512_and_accumulate(s, r, t):
     # multiply r by x^512, reduce mod x^821-1, add to s
-    s1,s2,s3,s4 = s
-    t5,t6,t7,t8 = t
+    s11, s1, s22, s2, s33, s3, s44, s4 = s
+    t55, t5, t66, t6, t77, t7, t88, t8 = t
+
+    r00, r0, r11, r1, r22, r2, r33, r3 = r
 
     # r[0] is aligned with 512:767
-    p("vpxor y{}, y{}, y{}".format(r[0], s3, s3))
+    # p("vpxor y{}, y{}, y{}".format(r[0], s3, s3))
+    p("vaddq_p128(y{}, y{}) = y{}".format(r00, s33, s33))
+    p("vaddq_p128(y{}, y{}) = y{}".format(r0, s3, s3))
 
+    # TODO
     # the low 53 of r[1] are aligned with 768:820
-    p("vpand low53(%rip), y{}, y{}".format(r[1], t8))
-    p("vpxor y{}, y{}, y{}".format(t8, s4, s4))
+    # p("vpand low53(%rip), y{}, y{}".format(r[1], t8))
+    p("0x1fffffffffffff = y{}".format(t88))
+    p("y{} & y{} = y{}".format(r1, t88, t8))
+    # r11 = t88
+    
+    # p("vpxor y{}, y{}, y{}".format(t8, s4, s4))
+    # r11 = t88
+    p("vaddq_p128(y{}, y{}) = y{}".format(r11, s44, s44))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t8, s4, s4))
+    
     # align the high 203 of r[1] with 0:202
-    vec256_sr53(t5, r[1], t8)
+    vec256_sr53((t55, t5), (r11, r1), t8, t88)
 
     # align low 53 of r[2] with 203:255
-    vec256_sl203(t6, r[2], t8)
+    vec256_sl203((t66, t6), (r22, r2), t8, t88)
     # align high 203 of r[2] with 256:458
-    vec256_sr53(t7, r[2], t8)
-    p("vpxor y{}, y{}, y{}".format(t5, t6, t6))
-    p("vpxor y{}, y{}, y{}".format(t6, s1, s1))
+    vec256_sr53((t77, t7), (r22, r2), t8, t88)
+    
+    # p("vpxor y{}, y{}, y{}".format(t5, t6, t6))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t55, t66, t66))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t5, t6, t6))
+
+    # p("vpxor y{}, y{}, y{}".format(t6, s1, s1))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t66, s11, s11))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t6, s1, s1))
 
     # align low 53 of r[3] with 459:511
-    vec256_sl203(t5, r[3], t8)
+    vec256_sl203((t55, t5), (r33, r3), t8, t88)
     # align high 203 of r[3] with 512:714
-    vec256_sr53(t6, r[3], t8)
-    p("vpxor y{}, y{}, y{}".format(t5, t7, t7))
-    p("vpxor y{}, y{}, y{}".format(t7, s2, s2))
-    p("vpxor y{}, y{}, y{}".format(t6, s3, s3))
+    vec256_sr53((t66, t6), (r33, r3), t8, t88)
+
+    # p("vpxor y{}, y{}, y{}".format(t5, t7, t7))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t55, t77, t77))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t5, t7, t7))
+
+    # p("vpxor y{}, y{}, y{}".format(t7, s2, s2))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t77, s22, s22))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t7, s2, s2))
+
+    # p("vpxor y{}, y{}, y{}".format(t6, s3, s3))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t66, s33, s33))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t6, s3, s3))
+
+    s = (s11, s1, s22, s2, s33, s3, s44, s4)
 
 def mul1024_and_accumulate(s, r, t):
     # multiply r by x^1024 (= x^203), reduce mod x^821 - 1, add to s
-    t5,t6,t7 = t
+    t55, t5, t66, t6, t77, t7 = t
+    s00, s0, s11, s1, s22, s2, s33, s3 = s 
+    r00, r0, r11, r1, r22, r2, r33, r3 = r 
+    # 0
+    # s[i] <- s[i] + "high 203 of r[i-1] | low 53 of r[i]"
+    vec256_sr53((t55, t5), (r33, r3), t7, t77)
+    vec256_sl203((t66, t6), (r00, r0), t7, t77)
+    # p("vpxor y{}, y{}, y{}".format(t5, t6, t6))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t55, t66, t66))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t5, t6, t6))
 
-    for i in [0,1,2,3]:
-      # s[i] <- s[i] + "high 203 of r[i-1] | low 53 of r[i]"
-      vec256_sr53(t5, r[i-1], t7)
-      vec256_sl203(t6, r[i], t7)
-      p("vpxor y{}, y{}, y{}".format(t5, t6, t6))
-      p("vpxor y{}, y{}, y{}".format(t6, s[i], s[i]))
+    # p("vpxor y{}, y{}, y{}".format(t6, s[i], s[i]))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t66, s00, s00))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t6, s0, s0))
+
+    # 1 
+    vec256_sr53((t55, t5), (r00, r0), t7, t77)
+    vec256_sl203((t66, t6), (r11, r1), t7, t77)
+    # p("vpxor y{}, y{}, y{}".format(t5, t6, t6))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t55, t66, t66))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t5, t6, t6))
+
+    # p("vpxor y{}, y{}, y{}".format(t6, s[i], s[i]))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t66, s11, s11))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t6, s1, s1))
+
+    # 2 
+    vec256_sr53((t55, t5), (r11, r1), t7, t77)
+    vec256_sl203((t66, t6), (r22, r2), t7, t77)
+    # p("vpxor y{}, y{}, y{}".format(t5, t6, t6))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t55, t66, t66))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t5, t6, t6))
+
+    # p("vpxor y{}, y{}, y{}".format(t6, s[i], s[i]))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t66, s22, s22))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t6, s2, s2))
+
+    # 3 
+    vec256_sr53((t55, t5), (r22, r2), t7, t77)
+    vec256_sl203((t66, t6), (r33, r3), t7, t77)
+    # p("vpxor y{}, y{}, y{}".format(t5, t6, t6))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t66, s11, s11))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t6, s1, s1))
+
+    p("vpxor y{}, y{}, y{}".format(t6, s[i], s[i]))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t66, s33, s33))
+    p("vaddq_p128(y{}, y{}) = y{}".format(t6, s3, s3))
+
 
 if __name__ == '__main__':
     p(".data")
@@ -256,14 +335,14 @@ if __name__ == '__main__':
     # rdi holds result, rsi holds a, rdx holds b
     # TODO: allow rdi=rsi
 
-    r=0,1,2,3
-    a,b=4,5
-    w,x=6,7
-    t1,t2,t3,t4=8,9,10,11
-    t5,t6,t7,t8=12,13,14,15
-    p("vmovdqa {}(%rsi), y{}".format( 0, a))
+    r = 0, 1, 2, 3
+    a, b = 4, 5
+    w, x = 6, 7
+    t1, t2, t3, t4 = 8, 9, 10, 11
+    t5, t6, t7, t8 = 12, 13, 14, 15
+    p("vmovdqa {}(%rsi), y{}".format(0, a))
     p("vmovdqa {}(%rsi), y{}".format(32, b))
-    p("vmovdqa {}(%rdx), y{}".format( 0, w))
+    p("vmovdqa {}(%rdx), y{}".format(0, w))
     p("vmovdqa {}(%rdx), y{}".format(32, x))
 
     karatsuba_512x512(r, (a, b), (w, x), t1, t2, t3, t4, t5, t6, t7)
@@ -272,12 +351,12 @@ if __name__ == '__main__':
     vec256_sr53(t1, r[3], t7)
     p("vpxor y{}, y{}, y{}".format(r[0], t1, t1))
     p("vpand low53(%rip), y{}, y{}".format(r[3], t2))
-    store_1024((t1,r[1],r[2],t2))
+    store_1024((t1, r[1], r[2], t2))
 
     # add r * x^512 mod x^821-1 to output
-    s = (a,b,w,x)
+    s = (a, b, w, x)
     load_1024(s, "%rdi")
-    mul512_and_accumulate(s, r, (t1,t2,t3,t4))
+    mul512_and_accumulate(s, r, (t1, t2, t3, t4))
     store_1024(s, "%rdi")
 
     c, d, y, z = 4, 5, 6, 7
@@ -288,30 +367,30 @@ if __name__ == '__main__':
 
     karatsuba_512x512(r, (c, d), (y, z), t1, t2, t3, t4, t5, t6, t7)
 
-    s = (c,d,y,z)
+    s = (c, d, y, z)
     load_1024(s, "%rdi")
-    mul512_and_accumulate(s, r, (t1,t2,t3,t4))
-    mul1024_and_accumulate(s, r, (t1,t2,t3))
+    mul512_and_accumulate(s, r, (t1, t2, t3, t4))
+    mul1024_and_accumulate(s, r, (t1, t2, t3))
     store_1024(s, "%rdi")
 
     # used all free registers during accumulate, reload inputs
-    a,b,w,x=4,5,6,7
-    c,d,y,z=8,9,10,11
-    t5,t6,t7,t8=12,13,14,15
-    load_1024((a,b,c,d), "%rsi")
-    load_1024((w,x,y,z), "%rdx")
+    a, b, w, x = 4, 5, 6, 7
+    c, d, y, z = 8, 9, 10, 11
+    t5, t6, t7, t8 = 12, 13, 14, 15
+    load_1024((a, b, c, d), "%rsi")
+    load_1024((w, x, y, z), "%rdx")
 
     p("vpxor y{}, y{}, y{}".format(a, c, a))
     p("vpxor y{}, y{}, y{}".format(b, d, b))
     p("vpxor y{}, y{}, y{}".format(w, y, w))
     p("vpxor y{}, y{}, y{}".format(x, z, x))
 
-    karatsuba_512x512(r, (a,b), (w, x), c, d, y, z, t5, t6, t7)
+    karatsuba_512x512(r, (a, b), (w, x), c, d, y, z, t5, t6, t7)
 
     # multiply by 512 and reduce mod x^821 - 1
-    s = (c,d,y,z)
+    s = (c, d, y, z)
     load_1024(s, "%rdi")
-    mul512_and_accumulate(s, r, (a,b,w,x))
+    mul512_and_accumulate(s, r, (a, b, w, x))
     store_1024(s, "%rdi")
 
     p("ret")
