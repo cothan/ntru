54cf6e0ec76da34c25c64f3fe6191d67c0f8bd48
pi64 <cothanhahaha123@gmail.com>
2020-03-03 23:35:27 -0500

    run on arm, fix bunch of things

--- ref-common/cpucycles.c
+++ ref-common/cpucycles.c
@@ -1,14 +1,36 @@
 #include "cpucycles.h"
+
+#if __aarch64__
+
+/*
+cpucycles/armv8.c version 20190803
+D. J. Bernstein
+Public domain.
+*/
+
 #include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
 
 long long cpucycles(void)
 {
-  #ifdef __arm__
-    clock_t result = clock();
-  #else
+  long long result = clock();
+  //asm volatile("mrs %0, PMCCNTR_EL0" : "=r" (result));
+  return result;
+}
+
+
+#else
+
+long long cpucycles(void)
+{
   unsigned long long result;
   asm volatile(".byte 15;.byte 49;shlq $32,%%rdx;orq %%rdx,%%rax"
     : "=a" (result) ::  "%rdx");
-  #endif
+
   return result;
 }
+
+
+#endif 
