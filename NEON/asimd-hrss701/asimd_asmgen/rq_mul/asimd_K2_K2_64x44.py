aa71b9c4616f01936c67fd6f41401a7c83daf19b
cothan <cothannguyen@gmail.com>
2020-03-26 15:13:42 -0400

    done control flow

--- NEON/asimd-hrss701/asimd_asmgen/rq_mul/asimd_K2_K2_64x44.py
+++ NEON/asimd-hrss701/asimd_asmgen/rq_mul/asimd_K2_K2_64x44.py
@@ -1,12 +1,31 @@
 from asimd_K2_schoolbook_64x11 import K2_schoolbook_64x11 as mul_64x11
 from asimd_tranpose import transpose48x16_to_16x44, transpose16x96_to_96x16
+from goto import with_goto
 
 p = print
 
+a_transpose = 0
+b_transpose = a_transpose + 44
+r_transpose = b_transpose + 44
+a_b_summed = r_transpose + 96
+a_in_rsp = a_b_summed + 22
+b_in_rsp = a_in_rsp + 22
+t2_in_rsp = b_in_rsp + 22
+coeffs = 44
+
+a_off, b_off, r_off = "", "", ""
+a_mem, b_mem, r_mem = "", "", ""
+a_real, b_real, r_real = "c", "a", "b"
+ecx = 4
+
+
 
 def karatsuba_loop(transpose=True):
-    global a_mem, b_mem, r_mem, a_off, b_off, r_off
+    global a_transpose, b_transpose, r_transpose
+    global a_off, b_off, r_off
+    global a_mem, b_mem, r_mem
     global a_real, b_real, r_real
+    global coeffs
 
     if transpose:
         a_off = a_transpose
@@ -21,7 +40,6 @@ def karatsuba_loop(transpose=True):
         transpose48x16_to_16x44(dst=a_mem, src=a_real, dst_off=a_off)
         transpose48x16_to_16x44(dst=b_mem, src=b_real, dst_off=b_off)
 
-        # TODO: is this need
         coeffs = 48
     else:
         a_off = b_off = r_off = 0
@@ -30,8 +48,11 @@ def karatsuba_loop(transpose=True):
         r_mem = r_real
 
 def innerloop(t0, t1, t2):
-    global a_mem, b_mem, r_mem, a_off, b_off, r_off
-    global a_real, b_real, r_real, a_b_summed
+    global a_transpose, b_transpose, r_transpose
+    global a_off, b_off, r_off
+    global a_mem, b_mem, r_mem
+    global a_real, b_real, r_real
+    global coeffs, ecx
 
     mul_64x11(r_mem, a_mem, b_mem, r_off, a_off, b_off)
     mul_64x11(r_mem, a_mem, b_mem, r_off + 22, a_off + 11, b_off + 11)
@@ -39,7 +60,7 @@ def innerloop(t0, t1, t2):
 
     slide = 0
     for j in range(2):
-        p("y{} = vld1q_u16({} + {});".format(t0[j], 16*(10 + a_b_summed) + slide, "rsp"))
+        p("y{} = vld1q_u16({} + rsp);".format(t0[j], 16*(10 + a_b_summed) + slide))
         p("y{} = vld1q_u16({} + {});".format(t1[j], 16*(10 + r_off) + slide, r_mem))
         p("y{} = vld1q_u16({} + {});".format(t2[j], 16*(32 + r_off) + slide, r_mem))
 
@@ -50,14 +71,14 @@ def innerloop(t0, t1, t2):
 
         slide = 8
 
-    for k in range(10):
+    for i in range(10):
         slide = 0
         for j in range(2):
             p("y{} = vld1q_u16({} + {});".format(t0[j], 16*(r_off + 11 + i) + slide, r_mem))
             p("y{} = vld1q_u16({} + {});".format(t2[j], 16*(r_off + 22 + i) + slide, r_mem))
             p("y{} = vsubq_u16(y{}, y{});".format(t0[j], t0[j], t2[j]))
 
-            p("y{} = vld1q_u16({} + {});".format(t1[j], 16*(11 + i + a_b_summed) + slide, "rsp"))
+            p("y{} = vld1q_u16({} + rsp);".format(t1[j], 16*(11 + i + a_b_summed) + slide))
             p("y{} = vsubq_u16(y{}, y{});".format(t1[j], t1[j], t0[j]))
 
             # t2 free
@@ -69,33 +90,41 @@ def innerloop(t0, t1, t2):
             p("y{} = vsubq_u16(y{} ,y{});".format(t0[j], t0[j], t2[j]))
 
             # t2 free
-            p("y{} = vld1q_u16({} + {});".format(t2[j], 16*(a_b_summed + i) + slide, "rsp"))
+            p("y{} = vld1q_u16({} + rsp);".format(t2[j], 16*(a_b_summed + i) + slide))
             p("y{} = vaddq_u16(y{} ,y{});".format(t0[j], t0[j], t2[j]))
 
             p("vst1q_u16({} + {}, y{});".format(16 *(11 + i + r_off) + slide, r_mem, t0[j]))
             p("vst1q_u16({} + {}, y{});".format(16 *(22 + i + r_off) + slide, r_mem, t1[j]))
 
             slide = 8
+    ecx = -ecx
 
 def done(t0, t1, t2):
     global a_mem, b_mem, r_mem, a_off, b_off, r_off
     global a_real, b_real, r_real, a_b_summed
     global a_in_rsp, b_in_rsp, t2_in_rsp
+    global ecx 
 
-    p("")
+    save = []
+
+    p("{} -= {};".format(a_mem, 16*22))
+    if a_mem != b_mem:
+        p("{} -= {};".format(b_mem, 16*22))
+    p("{} -= {};".format(r_mem, 16*44))
 
     for i in range(22):
         slide = 0
         for j in range(2):
             p("y{} = vld1q_u16({} + {});".format(t0[j], 16*(i + a_off) + slide, a_mem))
             p("y{} = vld1q_u16({} + {});".format(t1[j], 16*(22 + i + a_off) + slide, a_mem))
-            p("y{} = vaddq_u16(y{}, y{});",format(t0[j], t0[j], t1[j]))
+            p("y{} = vaddq_u16(y{}, y{});".format(t0[j], t0[j], t1[j]))
             p("vst1q_u16({} + rsp, y{});".format(16*(a_in_rsp + i) + slide, t0[j]))
+            # t0, t1 is free
 
-            p("y{} = vld1q_u16({} + {});".format(t2[j], 16*(i + b_off) + slide, b_mem))
-            p("y{} = vld1q_u16({} + {});".format(t3[j], 16*(22 + i + b_off) + slide, b_mem))
-            p("y{} = vaddq_u16(y{}, y{});",format(t2[j], t2[j], t3[j]))
-            p("vst1q_u16({} + rsp, y{});".format(16*(b_in_rsp + i) + slide, t2[j]))
+            p("y{} = vld1q_u16({} + {});".format(t0[j], 16*(i + b_off) + slide, b_mem))
+            p("y{} = vld1q_u16({} + {});".format(t1[j], 16*(22 + i + b_off) + slide, b_mem))
+            p("y{} = vaddq_u16(y{}, y{});".format(t0[j], t0[j], t1[j]))
+            p("vst1q_u16({} + rsp, y{});".format(16*(b_in_rsp + i) + slide, t0[j]))
 
             slide = 8 
 
@@ -117,9 +146,9 @@ def done(t0, t1, t2):
 
             # t2 free
             p("y{} = vld1q_u16({} + {});".format(t2[j], 16*(t2_in_rsp + 33 + i), "rsp"))
-            # t3 save for later 
-            p("y{} = vsubq_u16(y{} ,y{});".format(t3[j], t1[j], 16 + i + j))
-            save.append(16 + i + j)
+            # 8 + 2*i + j save for later
+            p("y{} = vsubq_u16(y{} ,y{});".format(8 + 2*i + j, t1[j], t2[j]))
+            save.append(8 + 2*i + j)
 
             # t2 free
             p("y{} = vld1q_u16({} + {});".format(t2[j], 16*(t2_in_rsp + i), "rsp"))
@@ -163,7 +192,13 @@ def done(t0, t1, t2):
             p("y{} = vsubq_u16(y{}, y{});".format(t0[j], t0[j], t1[j]))
 
             # r->coeffs[44 + i] = t2b.coeffs[22 + i] - r->coeffs[22 + i];
-            p("y{} = vsubq_u16(y{}, y{});".format(t1[j], save[2*i + j], t0[j]))
+            # print (save, 2*i + j)
+            if 2*i + j < 20: 
+                p("y{} = vsubq_u16(y{}, y{});".format(t1[j], save[2*i + j], t0[j]))
+            else:
+                p("y{} = vld1q_u16({} + rsp);".format(t2[j], 16*(t2_in_rsp + 22 +i)))
+                p("y{} = vsubq_u16(y{}, y{});".format(t1[j], t2[j], t0[j]))
+                
 
             # r->coeffs[44 + i] -= r->coeffs[66 + i];
             p("y{} = vld1q_u16({} + {});".format(t2[j], 16*(66 + i + r_off) + slide, r_mem))
@@ -190,31 +225,57 @@ def done(t0, t1, t2):
     if transpose: 
         transpose16x96_to_96x16(dst=r_real, src=r_mem, src_off=r_transpose)
 
+    p("{} += {};".format(a_real, 16*coeffs))
+    p("{} += {};".format(b_real, 16*coeffs))
+    p("{} += {};".format(r_real, 32*coeffs))
+
+    ecx -= 1
     
 
+@with_goto
+def K2_K2_transpose_64x44(transpose_input=True):
+    global transpose
+    transpose = transpose_input
 
-def K2_K2_transpose_64x44(r_real='%rdi', a_real='%rsi', b_real='%rdx', coeffs=44, transpose=True):
-    a_transpose = 0
-    b_transpose = a_transpose + 44
-    r_transpose = b_transpose + 44
-    a_b_summed = r_transpose + 96
-    a_in_rsp = a_b_summed + 22
-    b_in_rsp = a_in_rsp + 22
-    t2_in_rsp = b_in_rsp + 22
-
-    p("uint16_t rsp[{}];".format((44 + 44 + 96 + 22 + 22 + 22 + 44) * 16))
+    p("uint16_t tmp[{}/2];".format((44 + 44 + 96 + 22 + 22 + 22 + 44) * 32))
+    p("uint16_t *rsp = tmp;")
     
     t0 = (0, 1)
     t1 = (2, 3)
     t2 = (4, 5)
     t3 = (6, 7)
 
-    ecx = 4
+    label .karatsuba_loop
+    p("// Karatsuba_loop")
+    karatsuba_loop(transpose_input)
 
-    while 
-
-    karatsuba_loop()
+    label .innerloop
+    p("// Innerloop")
     innerloop(t0, t1, t2)
-    ecx = -ecx
-    if ecx == 0: 
-        done(t0, t1, t2)
+
+    if ecx >= 0:
+        goto .done
+
+    p("{} += {};".format(a_mem, 16*22))
+    if a_mem != b_mem:
+        p("{} += {};".format(b_mem, 16*22))
+    p("{} += {};".format(r_mem, 16*44))
+    goto .innerloop
+
+    label .done
+    p('// done')
+    done(t0, t1, t2)
+    
+
+    if ecx != 0:
+        goto .karatsuba_loop
+
+    #TODO: Skips this 
+    # p("{} -={};".format(r_real, 2 * (2*16 * coeffs*2)))
+    # p("rsp += {}".format( (44 + 44 + 96 + 22 + 22 + 22 + 44) * 16))
+
+
+if __name__ == "__main__":
+
+    K2_K2_transpose_64x44(transpose_input=True)
+
