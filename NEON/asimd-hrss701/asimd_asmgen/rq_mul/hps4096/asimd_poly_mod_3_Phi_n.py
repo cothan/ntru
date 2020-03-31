052f8db38d57f999713d70ddb12237c8132682ca
cothan <cothannguyen@gmail.com>
2020-03-31 13:15:51 -0400

    reorder print

--- NEON/asimd-hrss701/asimd_asmgen/rq_mul/hps4096/asimd_poly_mod_3_Phi_n.py
+++ NEON/asimd-hrss701/asimd_asmgen/rq_mul/hps4096/asimd_poly_mod_3_Phi_n.py
@@ -25,27 +25,27 @@ p(" uint16x8_t y9, y10, y11, y12, y13, y14, y15; ")
 
 
 # three = 3
-p("vdupq_n_s16(0x3) = y{}".format(x3))
+p("y{} = vdupq_n_s16(0x3);".format(x3))
 # xff 
-p("vdupq_n_u16(0xff) = y{}".format(xff))
+p("y{} = vdupq_n_u16(0xff);".format(xff))
 # xf 
-p("vdupq_n_u16(0xf) = y{}".format(xf))
+p("y{} = vdupq_n_u16(0xf);".format(xf))
 
 # Get last
-p("vld1q_u16 ({} + {}) = y{}".format((NTRU_N32//8 - 1)*8, "r->coeffs", last))
+p("y{} = vld1q_u16 ({} + {});".format(last, (NTRU_N32//8 - 1)*8, "r->coeffs"))
 # Extract 5th from last and duplicate all register
-p("vdupq_laneq_u16 (y{}, {}) = y{}".format(last, 5, last))
+p("y{} = vdupq_laneq_u16 (y{}, {});".format(last, last, 5))
 # last = last << 1
-p("vshlq_n_u16 (y{}, {}) = y{}".format(last, 1, last))
+p("y{} = vshlq_n_u16 (y{}, {});".format(last, last, 1))
 
 for i in range(0, NTRU_N32//16):
     p("// {} -> {}".format(i*16, (i+1)*16))
     # Load
-    p("vld1q_u16 ({} + {}) = y{}".format(i*16, "r->coeffs", r[0]))
-    p("vld1q_u16 ({} + {}) = y{}".format(i*16 + 8, "r->coeffs", r[1]))
+    p("y{} = vld1q_u16 ({} + {});".format(r[0], i*16, "r->coeffs"))
+    p("y{} = vld1q_u16 ({} + {});".format(r[1], i*16 + 8, "r->coeffs"))
     # r = r + last 
-    p("vaddq_u16 (y{}, y{}) = y{}".format(r[0], last, r[0]))
-    p("vaddq_u16 (y{}, y{}) = y{}".format(r[1], last, r[1]))
+    p("y{} = vaddq_u16 (y{}, y{});".format(r[0], r[0], last))
+    p("y{} = vaddq_u16 (y{}, y{});".format(r[1], r[1], last))
     # r = mod3(r)
     p("// MOD3 ")
     mod3(r, retval, t, c, xff, xf, x3, length=2)
@@ -58,7 +58,7 @@ for i in range(NTRU_N, NTRU_N32):
     # 701
     # 702
     # 703
-    p("0 = r->coeffs[{}]".format(i))
+    p("r->coeffs[{}] = 0;".format(i))
 
 p("}")
 
