e3825c97af28365e5878aefca91a8069b0196238
cothan <cothannguyen@gmail.com>
2020-03-31 13:30:30 -0400

    reorder print

--- NEON/asimd-hrss701/asimd_asmgen/rq_mul/hps4096/asimd_poly_mod_q_Phi_n.py
+++ NEON/asimd-hrss701/asimd_asmgen/rq_mul/hps4096/asimd_poly_mod_q_Phi_n.py
@@ -14,22 +14,22 @@ p("void poly_mod_q_Phi_n(poly *r){")
 p(" uint16x8_t y0, y1, y2, y3, y4, y5, y6, y7; ")
 
 # 0 
-p("vdupq_n_u16(0) = y{}".format(zero))
+p("y{} = vdupq_n_u16(0);".format(zero))
 
 
 # Get last
-p("vld1q_u16 ({} + {}) = y{}".format((NTRU_N32//8 - 1)*8, "r->coeffs", last))
+p("y{} = vld1q_u16 ({} + {});".format(last, (NTRU_N32//8 - 1)*8, "r->coeffs"))
 # Extract 5th from last and duplicate all register
-p("vdupq_laneq_u16 (y{}, {}) = y{}".format(last, 5, last))
+p("y{} = vdupq_laneq_u16 (y{}, {});".format(last, last, 5))
 
 for i in range(0, NTRU_N32//16):
     p("// {} -> {}".format(i*16, (i+1)*16))
     # Load
-    p("vld1q_u16 ({} + {}) = y{}".format(i*16, "r->coeffs", r[0]))
-    p("vld1q_u16 ({} + {}) = y{}".format(i*16 + 8, "r->coeffs", r[1]))
+    p("y{} = vld1q_u16 ({} + {});".format(r[0], i*16, "r->coeffs"))
+    p("y{} = vld1q_u16 ({} + {});".format(r[1], i*16 + 8, "r->coeffs"))
     # r = r - last 
-    p("vsubq_u16 (y{}, y{}) = y{}".format(r[0], last, r[0]))
-    p("vsubq_u16 (y{}, y{}) = y{}".format(r[1], last, r[1]))
+    p("y{} = vsubq_u16 (y{}, y{});".format(r[0], r[0], last))
+    p("y{} = vsubq_u16 (y{}, y{});".format(r[1], r[1], last))
     
     p("vst1q_u16 ({} + {}, y{});".format(i*16 , "r->coeffs"   , r[0]))
     p("vst1q_u16 ({} + {}, y{});".format(i*16 + 8, "r->coeffs", r[1]))
@@ -39,7 +39,7 @@ for i in range(NTRU_N, NTRU_N32):
     # 701
     # 702
     # 703
-    p("0 = r->coeffs[{}]".format(i))
+    p("r->coeffs[{}] = 0;".format(i))
 
 p("}")
 
