ab66e5258b68b88775ac50e5e6414ff6967e8e34
cothan <cothannguyen@gmail.com>
2020-03-31 17:11:35 -0400

    remove duplicate sample_iid

--- ref-common/sample_iid.c
+++ ref-common/sample_iid.c
@@ -16,12 +16,12 @@ static uint16_t mod3(uint16_t a)
   return (c&r) ^ (~c&t);
 }
 
-void sample_iid(poly *r, const unsigned char uniformbytes[NTRU_SAMPLE_IID_BYTES])
-{
-  int i;
-  /* {0,1,...,255} -> {0,1,2}; Pr[0] = 86/256, Pr[1] = Pr[-1] = 85/256 */
-  for(i=0; i<NTRU_N-1; i++)
-    r->coeffs[i] = mod3(uniformbytes[i]);
+// void sample_iid(poly *r, const unsigned char uniformbytes[NTRU_SAMPLE_IID_BYTES])
+// {
+//   int i;
+//   /* {0,1,...,255} -> {0,1,2}; Pr[0] = 86/256, Pr[1] = Pr[-1] = 85/256 */
+//   for(i=0; i<NTRU_N-1; i++)
+//     r->coeffs[i] = mod3(uniformbytes[i]);
 
-  r->coeffs[NTRU_N-1] = 0;
-}
+//   r->coeffs[NTRU_N-1] = 0;
+// }
