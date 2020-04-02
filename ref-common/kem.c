d87c1293f89542cff6d44610cbdda3a0901355a3
cothan <cothannguyen@gmail.com>
2020-04-02 11:52:20 -0400

    TODO: add SHA3 NEON

--- ref-common/kem.c
+++ ref-common/kem.c
@@ -30,7 +30,12 @@ int crypto_kem_enc(unsigned char *c, unsigned char *k, const unsigned char *pk)
 
   poly_S3_tobytes(rm, &r);
   poly_S3_tobytes(rm+NTRU_PACK_TRINARY_BYTES, &m);
+
+#if __aarch64__
   crypto_hash_sha3256(k, rm, NTRU_OWCPA_MSGBYTES);
+#else 
+  crypto_hash_sha3256(k, rm, NTRU_OWCPA_MSGBYTES);
+#endif 
 
   poly_Z3_to_Zq(&r); // XXX: Should be in owcpa_enc
   owcpa_enc(c, &r, &m, pk);
@@ -52,16 +57,23 @@ int crypto_kem_dec(unsigned char *k, const unsigned char *c, const unsigned char
   fail |= owcpa_dec(rm, c, sk);
   /* If fail = 0 then c = Enc(h, rm), there is no need to re-encapsulate. */
   /* See comment in owcpa_dec for details.                                */
-
+#if __aarch64__
   crypto_hash_sha3256(k, rm, NTRU_OWCPA_MSGBYTES);
+#else 
+  crypto_hash_sha3256(k, rm, NTRU_OWCPA_MSGBYTES);
+#endif 
 
   /* shake(secret PRF key || input ciphertext) */
   for(i=0;i<NTRU_PRFKEYBYTES;i++)
     buf[i] = sk[i+NTRU_OWCPA_SECRETKEYBYTES];
   for(i=0;i<NTRU_CIPHERTEXTBYTES;i++)
     buf[NTRU_PRFKEYBYTES + i] = c[i];
+  
+#if __aarch64__
   crypto_hash_sha3256(rm, buf, NTRU_PRFKEYBYTES+NTRU_CIPHERTEXTBYTES);
-
+#else
+  crypto_hash_sha3256(rm, buf, NTRU_PRFKEYBYTES+NTRU_CIPHERTEXTBYTES);
+#endif 
   cmov(k, rm, NTRU_SHAREDKEYBYTES, fail);
 
   return 0;
