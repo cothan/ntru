6759e453965f4a8bc45c409ad7f48f3c247ac6ed
cothan <cothannguyen@gmail.com>
2020-08-18 21:19:15 -0400

    ready to intergrate

--- neon-hps2048509/neon_poly_rq_mul.c
+++ neon-hps2048509/neon_poly_rq_mul.c
@@ -1,9 +1,8 @@
 #include "rq_mul/neon_poly_rq_mul.h"
 #include "poly.h"
 
-void neon_poly_Rq_mul(poly *r, const poly *a, const poly *b)
-// void poly_Rq_mul(poly *r, const poly *a, const poly *b)
+// void neon_poly_Rq_mul(poly *r, const poly *a, const poly *b)
+void poly_Rq_mul(poly *r, const poly *a, const poly *b)
 {
     poly_mul_neon(r->coeffs, a->coeffs, b->coeffs);
 }
-
