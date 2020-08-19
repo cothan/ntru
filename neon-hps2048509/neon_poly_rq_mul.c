f6ab1fa89b8b7fef37b3c83e622d8669543a8940
cothan <cothannguyen@gmail.com>
2020-08-19 17:44:37 -0400

    zero garbage

--- neon-hps2048509/neon_poly_rq_mul.c
+++ neon-hps2048509/neon_poly_rq_mul.c
@@ -2,7 +2,19 @@
 #include "poly.h"
 
 // void neon_poly_Rq_mul(poly *r, const poly *a, const poly *b)
-void poly_Rq_mul(poly *r, const poly *a, const poly *b)
+void poly_Rq_mul(poly *r, poly *a, poly *b)
 {
+    // Must zero garbage data at the end
+    a->coeffs[NTRU_N] = 0;
+    a->coeffs[NTRU_N+1] = 0;
+    a->coeffs[NTRU_N+2] = 0;
+    b->coeffs[NTRU_N] = 0;
+    b->coeffs[NTRU_N+1] = 0;
+    b->coeffs[NTRU_N+2] = 0;
+    
     poly_mul_neon(r->coeffs, a->coeffs, b->coeffs);
+    
+    r->coeffs[NTRU_N] = 0;
+    r->coeffs[NTRU_N+1] = 0;
+    r->coeffs[NTRU_N+2] = 0;
 }
--- neon-hps2048509/poly.h
+++ neon-hps2048509/poly.h
@@ -10,7 +10,7 @@ typedef struct{
   // round to nearest multiple of 32 to make it easier to load into vector
   //   registers without having to do bound checks
   #define NTRU_N_32 PAD32(NTRU_N)
-  uint16_t coeffs[NTRU_N_32] __attribute__((aligned(32)));
+  uint16_t coeffs[NTRU_N_PAD] __attribute__((aligned(32)));
 } poly;
 
 void poly_mod_3_Phi_n(poly *r);
@@ -26,7 +26,7 @@ void poly_S3_tobytes(unsigned char msg[NTRU_PACK_TRINARY_BYTES], const poly *r);
 void poly_S3_frombytes(poly *r, const unsigned char msg[NTRU_PACK_TRINARY_BYTES]);
 
 void poly_Sq_mul(poly *r, const poly *a, const poly *b);
-void poly_Rq_mul(poly *r, const poly *a, const poly *b);
+void poly_Rq_mul(poly *r, poly *a, poly *b);
 void poly_S3_mul(poly *r, const poly *a, const poly *b);
 void poly_lift(poly *r, const poly *a);
 void poly_Rq_to_S3(poly *r, const poly *a);
