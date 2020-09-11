b9c0a0636eab4e9b80b1bca031c57462e576b8d4
cothan <cothannguyen@gmail.com>
2020-09-11 16:30:32 -0400

    add CRYPTO_NAMESPACE for supercop, remove redundant files

--- supercop/crypto_kem/ntruhps2048509/neon/api.h
+++ supercop/crypto_kem/ntruhps2048509/neon/api.h
@@ -7,23 +7,7 @@ Joost Rijneveld, Tsunekazu Saito, John M. Schanck, Peter Schwabe,
 William Whyte,Keita Xagawa, Takashi Yamakawa, Zhenfei Zhang.
 =============================================================================*/
 
-#ifndef API_H
-#define API_H
-
-#include "params.h"
-
-#define CRYPTO_SECRETKEYBYTES NTRU_SECRETKEYBYTES
-#define CRYPTO_PUBLICKEYBYTES NTRU_PUBLICKEYBYTES
-#define CRYPTO_CIPHERTEXTBYTES NTRU_CIPHERTEXTBYTES
-#define CRYPTO_BYTES NTRU_SHAREDKEYBYTES
-
-#define CRYPTO_ALGNAME "ntruhps2048509"
-
-int crypto_kem_keypair(unsigned char *pk, unsigned char *sk);
-
-int crypto_kem_enc(unsigned char *ct, unsigned char *ss, const unsigned char *pk);
-
-int crypto_kem_dec(unsigned char *ss, const unsigned char *ct, const unsigned char *sk);
-
-
-#endif
+#define CRYPTO_PUBLICKEYBYTES 699
+#define CRYPTO_SECRETKEYBYTES 935
+#define CRYPTO_CIPHERTEXTBYTES 699
+#define CRYPTO_BYTES 32
